a962e229cd6e8107802c81e7c4844ef545725107
Virjanand Panday <virjanand@virjanand.com>
2022-07-09 18:47:21 +0200

    Refactor: get 100 numbers as Strings

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -6,7 +6,10 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
+    public static final int FIZZ_BUZZ_UNTIL = 100;
+    public static final int FIZZ_BUZZ_FROM = 1;
+
     public List<String> getNumbers() {
-        return IntStream.rangeClosed(1, 100).mapToObj(Integer::toString).collect(Collectors.toList());
+        return IntStream.rangeClosed(FIZZ_BUZZ_FROM, FIZZ_BUZZ_UNTIL).mapToObj(Integer::toString).collect(Collectors.toList());
     }
 }
